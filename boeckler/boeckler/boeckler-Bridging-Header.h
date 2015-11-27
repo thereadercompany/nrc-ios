7a1461a3bc3f7f2ed5319c7c1a478d9c0ee1306e
Emiel van der Veen <emiel@applike.nl>
2015-11-27 14:59:45 +0100

    Renamed trc-core project

--- boeckler/boeckler/boeckler-Bridging-Header.h
+++ boeckler/boeckler/boeckler-Bridging-Header.h
@@ -2,4 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#include "ios-nrc-nl-Bridging-Header.h"
+#include "trc-core-Bridging-Header.h"
