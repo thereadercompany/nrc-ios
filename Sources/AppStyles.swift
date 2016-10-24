0c2ea65bd3bb20e68d40b696f36487d30a4a7b51
Taco <t.vollmer@nrc.nl>
2016-10-24 09:09:46 +0200

    deleted unused styling code

--- Sources/AppStyles.swift
+++ Sources/AppStyles.swift
@@ -12,55 +12,19 @@ import Core
 import NRCFonts
 
 struct ContextStyles {
-//    static let endOfContentThreshold: CGFloat = 1.0
-//    static let hrefUnderlineStyle: NSUnderlineStyle = NSUnderlineStyle.StyleSingle
-//    static let pushAnimationDuration: NSTimeInterval = 0.54
     static let pushAnimationFadeInDuration: NSTimeInterval = 0.2
-//    static let popAnimationDuration: NSTimeInterval = 0.34
-//    static let popAnimationDurationXL: NSTimeInterval = 0.4
 }
 
 struct TimelineStyles {
     static let navBarAutoHideEnabled = false
-//    static let enablePullToRefresh = true
-//    static let topInset: CGFloat = 85
-//    static let defaultMargin: CGFloat = 0
-//    static let internalMargin: CGFloat = 16
     static let contentInset: CGFloat = 15
-//    static let backgroundColorBoot = Colors.timelineBackgroundColor
-//    static let backgroundColor = Colors.timelineBackgroundColor
-//    static let navigationBarHeight: CGFloat = 85 //65 + 20 statusbar
-//    static let lineInset: CGFloat = 16
-//    static let lineHeight: CGFloat = 1
-//    static let initiallyHideNavigationView = false
     static let navigationViewStyle = NavigationViewStyle.Dark(Colors.navigationViewLightBackgroundColor)
-//    static func navigationViewNeedsLine(style: NavigationViewStyle) -> Bool {
-//        return true
-//    }
-//    static func navigationTextVisible(style: NavigationViewStyle) -> Bool {
-//        switch style {
-//        case .Transparent, .Dark:
-//            return false
-//        case .Light:
-//            return true
-//        }
-//    }
-    
-//    static func preferredStatusBarStyle(navStyle: NavigationViewStyle) -> UIStatusBarStyle {
-//        switch navStyle {
-//        case .Dark:
-//            return .LightContent
-//        case .Transparent, .Light:
-//            return .Default
-//        }
-//    }
 }
 
 struct ArticleStyles {
     static let topInset: CGFloat = 0
     static let navBarAutoHideEnabled = true
     static let navigationBarHeight: CGFloat = 85 //65 + 20 statusbar
-    static var textInset: CGFloat = 20
     static let pushToHideThreshold: CGFloat = 120
     static let backgroundColor = Colors.accentColor
     
@@ -96,6 +60,7 @@ extension NSAttributedString {
         let fixedString = "<\(style.rawValue)>" + string + "</\(style.rawValue)>"
         if let data = fixedString.dataUsingEncoding(NSUTF8StringEncoding) where data.length > 0 {
             let attributedString = NSMutableAttributedString(HTMLData: data, options: attributes.dtOptions)
+    static var textInset: CGFloat = 20
             
             guard attributedString.length > 0 else {
                 self.init(attributedString: attributedString)
